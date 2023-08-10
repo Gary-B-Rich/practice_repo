e15e3e63b77bf2f7ff83fe3880c43f384802feb4
Gary-B-Rich <garybrich@hotmail.com>
2023-08-10 12:39:40 -0400

    javaScript button practice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,17 @@
-console.log("This is my javaScript page");
+
+const btn = document.querySelector("button");
+const txt = document.querySelector("p");
+
+btn.addEventListener("click", updateBtn);
+
+function updateBtn() {
+  if (btn.textContent === "Start machine") {
+    btn.textContent = "Stop machine";
+    txt.textContent = "The machine has started!";
+  } else {
+    btn.textContent = "Start machine";
+    txt.textContent = "The machine is stopped.";
+  }
+}
+
+
