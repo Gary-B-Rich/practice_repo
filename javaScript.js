c5e0828431f5cf5cbbc3ce800e856186f996cbba
Gary-B-Rich <garybrich@hotmail.com>
2023-08-11 10:12:45 -0400

    more JS

--- javaScript.js
+++ javaScript.js
@@ -16,3 +16,21 @@ const max = 57;
 let actual = max - 13;
 let percentage = actual / max;
 console.log(percentage);
+
+const string = "The revolution will not be televised.";
+console.log(string);
+
+//const badString1 = This is a test;
+//const badString2 = 'This is a test;
+//const badString3 = This is a test';
+
+const badString = string;
+console.log(badString);
+
+// Backslash inside of single or double quotes
+// causes the compiler to ignore it as code, and instead
+// regard it as text.  This is known as an 'escape character.'
+const bigMouth = 'I\'ve got no right to take my place...';
+console.log(bigMouth);
+ 
+
