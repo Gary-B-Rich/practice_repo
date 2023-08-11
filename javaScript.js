60eaf1ca66625685b9bf436b2c942dfa81d2bec8
Gary-B-Rich <garybrich@hotmail.com>
2023-08-11 10:52:48 -0400

    mdn js practice

--- javaScript.js
+++ javaScript.js
@@ -32,5 +32,47 @@ console.log(badString);
 // regard it as text.  This is known as an 'escape character.'
 const bigMouth = 'I\'ve got no right to take my place...';
 console.log(bigMouth);
- 
 
+const name = "Chris";
+const greeting = `Hello, ${name}`;
+console.log(greeting); //Hello, Chris
+
+
+const one = "Hello, ";
+const two = "how are you?";
+const joined = `${one}${two}`;
+console.log(joined); //Hello, how are you?
+
+
+const button = document.querySelector("button");
+function greet() {
+    const name = prompt("What is your name?");
+    alert(`Hello ${name}, nice to see you!`);
+}
+button.addEventListener("click", greet);
+
+
+const street = "Front ";
+const addy = 242;
+console.log(`${street}${addy}`); //Front 242
+
+
+const myString = "123";
+const myNum = Number(myString);
+console.log(typeof myNum);
+console.log(myNum);
+
+
+const myNum2 = 123;
+const myString2 = myNum2.toString();
+console.log(typeof myString2);
+console.log(myString2);
+
+
+const song = "Fight the Youth";
+const score = 9;
+const highestScore = 10;
+const output = `I like the song ${song}.  I gave it a score of ${
+    (score / highestScore) * 100
+}%.`;
+console.log(output); 
