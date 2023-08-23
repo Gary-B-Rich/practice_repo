f357bc775b641524b8ace9d3cc1b2d2e406e283a
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2023-08-23 15:32:05 -0400

    test function parameters

---
+++ code_test/JS.js
@@ -0,0 +1,10 @@
+function favFood(response) {
+    if(response==undefined) {
+        console.log("Please enter your favorite food!");
+    } else {
+    console.log(`Your favorite food is ${response}.`);
+    }
+};
+
+//favFood();
+favFood("beef");
