cb1d76b7a9f994d2f097b4e6f867c320db197fb7
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2023-08-26 12:40:03 -0400

    messing around

--- JS_practice/misc/traverse_dom/script.js
+++ JS_practice/misc/traverse_dom/script.js
@@ -1,2 +1,15 @@
 // script.js for traverse_dom repo
 
+const grandparent = document.getElementById("grandparent-id");
+const parents = document.getElementsByClassName('parent');
+
+parents.forEach(changeColor);
+
+//inline background styling:
+//grandparent.style.backgroundColor = "#333";
+
+//funtion to do the same thing:
+//changeColor(grandparent);
+//function changeColor(element) {
+    //element.style.backgroundColor = "#333";
+//}
