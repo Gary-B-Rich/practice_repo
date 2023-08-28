2d0aa720af664676b97f9e4e4fc211653889414e
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2023-08-28 12:32:03 -0400

    finish practice exercises

--- JS_practice/misc/dom-practice/js-file.js
+++ JS_practice/misc/dom-practice/js-file.js
@@ -5,7 +5,24 @@ para.style.color = "Red";
 document.body.appendChild(para);
 
 
+// Create an <h3> with blue text, and append it 
 const h3title = document.createElement("h3");
 h3title.innerText = "I'm a blue h3!";
 h3title.style.color = "Blue";
 document.body.appendChild(h3title);
+
+
+// Create a <div> with blk border & pink backround.  DON'T append.
+const div1 = document.createElement("div");
+div1.style.border = "2px solid black";
+div1.style.backgroundColor = "pink";
+    // Create <h1> append inside <div>
+    const h1title = document.createElement("h1");
+    h1title.innerText = "I'm in a div";
+    div1.appendChild(h1title);
+    // Create <p> append inside <div>
+    const para2 = document.createElement("p");
+    para2.innerText = "ME TOO!";
+    div1.appendChild(para2);
+// Append <div> with <h1> & <p> to document
+document.body.appendChild(div1);
