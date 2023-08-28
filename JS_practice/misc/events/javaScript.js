73d9a57e40edbafeda628570a58b6a97f8c2b2b6
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2023-08-28 13:17:59 -0400

    more practice

--- JS_practice/misc/events/javaScript.js
+++ JS_practice/misc/events/javaScript.js
@@ -1,1 +1,11 @@
 //javaScript.js for events directory
+
+// .js for method two
+const btn = document.querySelector('#btn');
+btn.onclick = () => alert("Hello World");
+
+// .js for method three
+const mtd3 = document.querySelector('#mtd3');
+mtd3.addEventListener('click', () => {
+    alert("Hello World");
+});
