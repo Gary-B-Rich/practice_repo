4630012a238715e7fe9a992ec59edb4bbe62bd73
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2024-02-09 13:01:24 -0500

    add last of code and notes

--- JS_practice/misc/arrowFunctions/script.js
+++ JS_practice/misc/arrowFunctions/script.js
@@ -42,6 +42,38 @@ let randomNumber2 = () => {
 let randomNumber3 = () => Math.random;
 
 //********************************************************
+// Normal 
 document.addEventListener('click', function() {
     console.log('click');
 })
+// Same with arrow
+document.addEventListener('click', () => {
+    console.log('click');
+})
+// Same when single line return
+document.addEventListener('click', () => console.log('click'));
+
+//**********************************************************
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    //Arrow Function
+    //Inside an arrow function 'this' has local scope
+    printNameArrow() {
+        setTimeout(() => {
+            console.log('Arrow: ' + this.name);
+        }, 100)
+    }
+    //Same as the Arrow Function, but standard.
+    //Inside a regular function 'this' has global scope from
+    //wherever the fuction was called
+    printNameFunction() {
+        setTimeout(function() {
+            console.log('Function: ' + this.name);
+        }, 100)
+    }
+}
+let person = new Person('Bob');
+person.printNameArrow();
+person.printNameFunction();
