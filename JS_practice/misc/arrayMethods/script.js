94a4499efcc288014a36f36c7459254558a8fa9a
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2024-02-09 10:39:45 -0500

    various array methods

--- JS_practice/misc/arrayMethods/script.js
+++ JS_practice/misc/arrayMethods/script.js
@@ -1,4 +1,6 @@
 // script.js for arrayMethods
+// tutorial video: https://www.youtube.com/watch?v=R8rmfD9Y5-c
+
 const items = [
     { name: 'Bike',     price: 100 },
     { name: 'TV',       price: 200 },
@@ -8,3 +10,54 @@ const items = [
     { name: 'Computer', price: 1000},
     { name: 'Keyboard', price: 25  }
 ]
+
+// original array
+console.log(items);
+// use .filter() method to create a new array of items with prices
+// less than or equal to 100
+const filteredItems = items.filter((item) => {
+    return item.price <= 100;
+})
+// the new filtered array
+console.log(filteredItems);
+//******************************************* 
+
+// .map() method for new array of items by name
+const itemNames = items.map((item) => {
+    return item.name;
+})
+// mapped array
+console.log(itemNames);
+//*******************************************
+
+// .filter() for specific item name
+const foundItem = items.filter((item) => {
+    return item.name === 'Book';
+})
+// filtered array
+console.log(foundItem);
+//*******************************************
+
+// .some() checks entire object for one case of param, 
+// returns true or false
+const hasInexpensiveItems = items.some((item) => {
+    return item.price <= 100;
+})
+console.log(hasInexpensiveItems);
+//*******************************************
+
+// .every() checks ALL object cases match
+const allInexpensiveItems = items.every((item) => {
+return item.price <= 100;
+}) 
+console.log(allInexpensiveItems);
+//*******************************************
+
+const total = items.reduce((currentTotal, item) => {
+    return item.price + currentTotal;
+}, 0)
+console.log(total);
+
+
+
+
