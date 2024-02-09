019ff19b213526dba3e57c8ab5f626f1e4108471
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2024-02-09 09:52:03 -0500

    add script

--- JS_practice/misc/arrayMethods/script.js
+++ JS_practice/misc/arrayMethods/script.js
@@ -1,1 +1,10 @@
 // script.js for arrayMethods
+const items = [
+    { name: 'Bike',     price: 100 },
+    { name: 'TV',       price: 200 },
+    { name: 'Album',    price: 10  },
+    { name: 'Book',     price: 5   },
+    { name: 'Phone',    price: 500 },
+    { name: 'Computer', price: 1000},
+    { name: 'Keyboard', price: 25  }
+]
