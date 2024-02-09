0015e67dc894562a969aed5d3e71cca333a26aa1
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2024-02-09 12:24:11 -0500

    add .js script

--- JS_practice/misc/arrowFunctions/script.js
+++ JS_practice/misc/arrowFunctions/script.js
@@ -1,8 +1,19 @@
 // script.js for arrowFunctions
 
+//*******************************************************
+// Three examples of the same thing:
+// Normal Function
 function sum(a, b) {
     return a + b;
 }
+// Arrow Function
+let sum2 = (a, b) => {
+    return a + b;
+}
+// Same Arrow Function
+let sum3 = (a, b) =>  a + b;
+//*******************************************************
+
 
 function isPositive(number) {
     return number >= 0;
