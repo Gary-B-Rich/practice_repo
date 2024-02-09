44b6c39b140c65b79cb777872b15fb207f6a176a
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2024-02-09 12:15:11 -0500

    add .js code

--- JS_practice/misc/arrowFunctions/script.js
+++ JS_practice/misc/arrowFunctions/script.js
@@ -1,2 +1,17 @@
 // script.js for arrowFunctions
 
+function sum(a, b) {
+    return a + b;
+}
+
+function isPositive(number) {
+    return number >= 0;
+}
+
+function randomNumber() {
+    return Math.random;
+}
+
+document.addEventListener('click', function() {
+    console.log('click');
+})
