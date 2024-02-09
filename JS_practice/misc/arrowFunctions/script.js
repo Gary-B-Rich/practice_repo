85fbc66a6b586eb81db4c92886ee81baacb4cfe6
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2024-02-09 12:33:32 -0500

    adding various examples

--- JS_practice/misc/arrowFunctions/script.js
+++ JS_practice/misc/arrowFunctions/script.js
@@ -1,4 +1,5 @@
 // script.js for arrowFunctions
+// video tutorial: https://www.youtube.com/watch?v=h33Srr5J9nY
 
 //*******************************************************
 // Three examples of the same thing:
@@ -10,15 +11,26 @@ function sum(a, b) {
 let sum2 = (a, b) => {
     return a + b;
 }
-// Same Arrow Function
+// Same Arrow Function when single line return
 let sum3 = (a, b) =>  a + b;
 //*******************************************************
 
-
+// Normal Function
 function isPositive(number) {
     return number >= 0;
 }
+// Arrow Function of the same thing
+// when using a single parameter: (number) parenthesis unneeded 
+let isPositive2 = number => {
+    return number >= 0;
+}
+// Same Arrow Function when single line return
+// single parameter (number) parenthesis optional 
+let isPositive3 = (number) => number >= 0;
 
+
+
+//********************************************************
 function randomNumber() {
     return Math.random;
 }
