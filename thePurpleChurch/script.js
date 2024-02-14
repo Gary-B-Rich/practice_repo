6a0601e7ee05cca44ad442200080b72898a55a4c
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2024-02-14 13:13:28 -0500

    add files to new folder

---
+++ thePurpleChurch/script.js
@@ -0,0 +1,27 @@
+//  script.js for the-deep directory
+
+const button1=document.getElementById('button1');
+button1.addEventListener("click", function() {
+    console.log("Clicked button 1");
+});
+
+const button2=document.getElementById('button2');
+button2.addEventListener("click", function() {
+    console.log("Clicked button 2");
+});
+
+const button3=document.getElementById('button3');
+button3.addEventListener("click", function() {
+    console.log("Clicked button 3");
+});
+
+const button4=document.getElementById('button4');
+button4.addEventListener("click", function() {
+    console.log("Clicked button 4");
+});
+
+const logo = document.querySelector('.logo');
+logo.innerText="Welcome to the Purple Church";
+
+const footer = document.querySelector('.footer');
+footer.innerText="This is the footer";
