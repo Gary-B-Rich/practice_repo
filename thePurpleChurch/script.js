1da25ad5a700c9c2926b16ca6be3a2ecac695744
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2024-03-27 14:44:54 -0400

    large text boxes added in .js

--- thePurpleChurch/script.js
+++ thePurpleChurch/script.js
@@ -4,17 +4,14 @@ const button1=document.getElementById('button1');
 button1.addEventListener("click", function() {
     console.log("You clicked button 1");
 });
-
 const button2=document.getElementById('button2');
 button2.addEventListener("click", function() {
     console.log("Clicked button 2");
 });
-
 const button3=document.getElementById('button3');
 button3.addEventListener("click", function() {
     console.log("Clicked button 3");
 });
-
 const button4=document.getElementById('button4');
 button4.addEventListener("click", function() {
     console.log("Clicked button 4");
@@ -24,13 +21,31 @@ const welcomeText = document.querySelector('.welcome');
 welcomeText.innerText="Welcome to the Purple Church";
 
 const blackBox1 = document.getElementById('blackBox1');
-blackBox1.innerText="This is blackBox1";
+blackBox1.innerText="Lorem ipsum dolor sit amet consectetur adipisicing \
+elit. Maxime, adipisci. Enim fugiat temporibus explicabo doloremque \
+debitis labore optio minima tenetur rerum modi, fuga consectetur \
+magnam, officia magni sed facilis hic incidunt itaque repudiandae! \
+Provident illum ipsam, soluta dignissimos alias voluptate dolorem \
+consequatur aperiam aliquam ut, ipsa ratione fugit asperiores \
+ipsum dolor sit amet consectetur adipisicing beatae?";
 
 const blackBox2 = document.getElementById('blackBox2');
-blackBox2.innerText="This is blackBox2";
+blackBox2.innerText="Lorem ipsum dolor sit amet consectetur adipisicing \
+elit. Maxime, adipisci. Enim fugiat temporibus explicabo doloremque \
+debitis labore optio minima tenetur rerum modi, fuga consectetur \
+magnam, officia magni sed facilis hic incidunt itaque repudiandae! \
+Provident illum ipsam, soluta dignissimos alias voluptate dolorem \
+consequatur aperiam aliquam ut, ipsa ratione fugit asperiores \
+ipsum dolor sit amet consectetur adipisicing beatae?";
 
 const pinkBox1 = document.getElementById('pinkBox1');
-pinkBox1.innerText="This is pinkBox1";
+pinkBox1.innerText="Lorem ipsum dolor sit amet consectetur adipisicing \
+elit. Maxime, adipisci. Enim fugiat temporibus explicabo doloremque \
+debitis labore optio minima tenetur rerum modi, fuga consectetur \
+magnam, officia magni sed facilis hic incidunt itaque repudiandae! \
+Provident illum ipsam, soluta dignissimos alias voluptate dolorem \
+consequatur aperiam aliquam ut, ipsa ratione fugit asperiores \
+ipsum dolor sit amet consectetur adipisicing beatae?";
 
 
 
