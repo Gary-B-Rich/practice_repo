ec41540577f697f8ac8f4cde91ed56fb4646e2fc
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2024-04-05 09:44:10 -0400

    change html

--- thePurpleChurch/script.js
+++ thePurpleChurch/script.js
@@ -8,7 +8,8 @@ document.documentElement.style.setProperty('--background', 'black');
 const button1=document.getElementById('button1');
 button1.addEventListener("click", function() {
     console.log("Clicked button 1");
-    document.documentElement.style.setProperty('--mainColor', 'yellow');
+    //change color variables on click
+    document.documentElement.style.setProperty('--mainColor', 'goldenrod');
     document.documentElement.style.setProperty('--background', 'black');
 });
 const button2=document.getElementById('button2');
