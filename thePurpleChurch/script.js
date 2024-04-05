29a15321adfa45110c2af1963d2cda5a3a4b7f6b
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2024-04-05 08:53:59 -0400

    change :root variables in css and start adding from .js

--- thePurpleChurch/script.js
+++ thePurpleChurch/script.js
@@ -1,4 +1,5 @@
 //  script.js for thePurpleChurch in practice_repo 
+document.documentElement.style.setProperty('--mainColor', 'hotpink');
 
 const button1=document.getElementById('button1');
 button1.addEventListener("click", function() {
